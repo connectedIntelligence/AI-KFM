caf09a4e8d0de07014cd44a994ddcec571ee950e
Shivam Pandey <pandeyshivam2017robotics@gmail.com>
2022-04-07 22:53:20 +0530

    Added training script, and instructions to run.
    Signed-off-by: Shivam Pandey <pandeyshivam2017robotics@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # AI-KFM
+
+## Training
+
+```shell
+python train.py <data_dir> --epochs <n> --batch_size <b> --lambda1 <l1> --lambda2 <l2> --results_dir <r_dict>
+```
--- aikfm/train.py
+++ aikfm/train.py
@@ -1,3 +1,6 @@
+import argparse
+import os
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -6,15 +9,41 @@ from torch.utils.data import DataLoader
 from aikfm.dataset import AikfmDataset
 from aikfm.models import CAN8, UCAN64, discriminator
 
-max_epoch_num = 30
-mini_batch_size = 20
-lambda1 = 100
-lambda2 = 10
+
+def save_checkpoint(state, filename='checkpoint'):
+    filename = '{}.pth'.format(filename)
+    torch.save(state, filename)
+
+def checkpoint_state(model=None, optimizer=None, epoch=None, it=None):
+    optim_state = optimizer.state_dict() if optimizer is not None else None
+    if model is not None:
+        if isinstance(model, torch.nn.DataParallel):
+            model_state = model.module.state_dict()
+        else:
+            model_state = model.state_dict()
+    else:
+        model_state = None
+
+    return {'epoch': epoch, 'it': it, 'model_state': model_state, 'optimizer_state': optim_state}
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_root", type=str, help="Data root directory, with Training and Test directories.")
+    parser.add_argument("--epochs", default=30, type=int, help="Max number of epochs the training should be performed.")
+    parser.add_argument("--batch_size", default=20, type=int, help="Mini-batch size to train the model.")
+    parser.add_argument("--lambad1", default=100.0, type=float)
+    parser.add_argument("--lambda2", default=10.0, type=float)
+    parser.add_argument("--results_dir", default=os.path.join(os.getenv('HOME'), 'ai-kfm-results'), type=str)
 
-    dataset = AikfmDataset("~/DKLabs/AI-KFM/AI-KFM/data")
-    dataloader = DataLoader(dataset, batch_size=mini_batch_size, shuffle=True)
+    args = parser.parse_args()
+
+    max_epoch_num = args.epochs
+    mini_batch_size = args.batch_size
+    lambda1 = args.lambda1
+    lambda2 = args.lambda2
+
+    dataset = AikfmDataset(args.data_root)
+    dataloader = DataLoader(dataset, batch_size=mini_batch_size, shuffle=True, num_workers=4)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -184,3 +213,10 @@ if __name__ == "__main__":
 
             gen_loss2.backward()
             optim_g2.step()
+
+            ckpt_name1 = os.path.join(args.results_dir, 'models/g1_epoch_{}_batch_{}'.format(epoch+1, bt_idx+1))
+            ckpt_name2 = os.path.join(args.results_dir, 'models/g2_epoch_{}_batch_{}'.format(epoch+1, bt_idx+1))
+            ckpt_name3 = os.path.join(args.results_dir, 'models/dis_epoch_{}_batch_{}'.format(epoch+1, bt_idx+1))
+            save_checkpoint(checkpoint_state(g1, optim_g1, epoch+1, it), filename=ckpt_name1)
+            save_checkpoint(checkpoint_state(g2, optim_g2, epoch+1, it), filename=ckpt_name2)
+            save_checkpoint(checkpoint_state(dis, optim_dis, epoch+1, it), filename=ckpt_name3)
